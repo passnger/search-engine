1702f71cea605da9f44b5c9a13e286a34a7745ed
passnger <passnger@email.com>
2016-08-23 19:04:15 +0200

    Initial commit

---
+++ lib/TargetsFinder.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/**
+ * TargetsFinder has for role to read a file that contains
+ * a list of URL (one by line) to scan.
+ * 
+ */
+class TargetsFinder
+{
+	/**
+	 * @var array
+	 * An array that contains all targets
+	 */
+	private $targets;
+
+
+	public function __construct($filePath = __DIR__."../res/targets.txt")
+	{
+		$targets = array();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+$test = new TargetsFinder();
